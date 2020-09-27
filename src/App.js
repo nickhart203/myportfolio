26738e77e48941d1fafed39c68cc4ef71405762d
nickhart00 <31710907+nickhart00@users.noreply.github.com>
2020-09-27 11:44:59 -0600

    add CNAME file and modify package

--- src/App.js
+++ src/App.js
@@ -16,7 +16,31 @@ import DisplayGroup from "./DisplayGroup.js";
 import USN from "./USN.js";
 import Meritus from "./Meritus.js";
 import Drumnow from "./Drumnow.js";
+import Thumbnail from "./Thumbnail.js";
+import usnThumb from "./assets/img/usn-thumbnail.jpg";
+import coachellaThumb from "./assets/img/coachella-thumbnail.jpg";
+import coalesseThumb from "./assets/img/coalesse-thumbnail.jpg";
+import displaygroupThumb from "./assets/img/displaygroup-thumbnail.jpg";
+import meritusThumb from "./assets/img/meritus-thumbnail.jpg";
+import drumnowThumb from "./assets/img/drumnow-thumbnail.jpg";
 import { motion } from "framer-motion";
+
+const list = {
+  visible: {
+    opacity: 1,
+    transition: { delayChildren: 0, staggerChildren: 0.2 },
+  },
+  hidden: { opacity: 0 },
+};
+
+const item = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { ease: "easeOut", duration: 0.65 },
+  },
+  hidden: { opacity: 0, y: 15 },
+};
 
 function App() {
   return (
@@ -34,11 +58,10 @@ function App() {
         >
           <Navbar />
         </motion.div>
-        
+
         <Social />
-        <Work />
         {/* router links*/}
-        <Route path="/" component={Work} exact />
+        <Route exact path="/" component={Work} />
         <Route path="/about" component={About} />
         <Route path="/resume" component={Resume} />
         <Route path="/us-nursing" component={USN} />
@@ -47,6 +70,88 @@ function App() {
         <Route path="/coalesse" component={Coalesse} />
         <Route path="/meritus" component={Meritus} />
         <Route path="/display-group" component={DisplayGroup} />
+
+        <div className="pageContainer">
+          <motion.ul initial="hidden" animate="visible" variants={list}>
+            <div className="intro-textContainer">
+              <div className="intro-text">
+                <h1>
+                  <motion.li variants={item}>I'm Nick. ✌️ I enjoy </motion.li>
+                  <motion.li variants={item}>
+                    interaction, motion, and{" "}
+                  </motion.li>
+                  <motion.li variants={item}>
+                    brand design. Let me know{" "}
+                  </motion.li>
+                  <motion.li variants={item}>if you want to chat! ✨</motion.li>
+                </h1>
+              </div>
+              <motion.li variants={item}>
+                <div className="intro-contact">
+                  <p>
+                    See what I'm doing on{" "}
+                    <a
+                      href="http://www.dribbble.com/cubeycap"
+                      className="line-link-sm"
+                      target="_blank"
+                    >
+                      dribbble.
+                    </a>
+                  </p>
+
+                  <p>
+                    Get in touch:{" "}
+                    <a
+                      href="mailto:hello.nickhart@gmail.com"
+                      className="line-link-sm"
+                    >
+                      hello.nickhart@gmail.com
+                    </a>
+                  </p>
+                </div>
+              </motion.li>
+            </div>
+
+            <motion.li variants={item}>
+              <Thumbnail
+                link="/us-nursing"
+                image={usnThumb}
+                title="US Nursing"
+                subtext="Product Design"
+              />
+              <Thumbnail
+                link="/coachella"
+                image={coachellaThumb}
+                title="Coachella 2020"
+                subtext="Visual Design & Animation"
+              />
+              <Thumbnail
+                link="/coalesse"
+                image={coalesseThumb}
+                title="Coalesse"
+                subtext="UX/UI Website Redesign"
+              />
+              <Thumbnail
+                link="/drumnow"
+                image={drumnowThumb}
+                title="Drumnow"
+                subtext="Product Design"
+              />
+              <Thumbnail
+                link="/meritus"
+                image={meritusThumb}
+                title="Meritus Communities"
+                subtext="UX Case Study"
+              />
+              <Thumbnail
+                link="/display-group"
+                image={displaygroupThumb}
+                title="Display Group"
+                subtext="UX/UI Website Redesign"
+              />
+            </motion.li>
+          </motion.ul>
+        </div>
 
         <Footer />
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,30 +7,21 @@ import { motion } from "framer-motion";
 function Navbar() {
   return (
     <div className="container">
-
-        <div className="navContainer">
-          <div className="logo line-link inline-block">
-            <Link classname="hue" to="/">
-              Nick Hart{" "}
+      <div className="navContainer">
+        <div className="logo line-link inline-block">
+          <Link to="/">Nick Hart </Link>
+        </div>
+        <div className="nav-items inline-block">
+          <div className="nav-item line-link">
+            <Link to="/about">About</Link>
+          </div>
+          <div className="nav-item line-link">
+            <Link to="./resume_nickhart.pdf" target="_blank">
+              <Download></Download>
             </Link>
           </div>
-          <div className="nav-items inline-block">
-            <div className="nav-item line-link">
-              <Link classname="" to="/about">
-                About
-              </Link>
-            </div>
-            <div className="nav-item line-link">
-              <Link
-                classname="line-link"
-                to="./resume_nickhart.pdf"
-                target="_blank"
-              >
-                <Download></Download>
-              </Link>
-            </div>
-          </div>
         </div>
+      </div>
     </div>
   );
 }
