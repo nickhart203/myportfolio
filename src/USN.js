514bbb8445f21054ab31a7f185869bd28f958fca
nickhart00 <31710907+nickhart00@users.noreply.github.com>
2020-09-30 18:26:21 -0600

    update to project page

--- src/USN.js
+++ src/USN.js
@@ -62,11 +62,12 @@ function USN(props) {
             header="Design system"
             body="I put together all the working parts of the design system inside Figma to foster consistency and to serve as a foundation for the new designs. This helped the visual design move forward without redundancy."
           />
-          <WorkHeroVideo video="https://player.vimeo.com/video/459954812?api=1&amp;autoplay=1&amp;background=1&amp;loop=1&amp;muted=1" />
+          <WorkHeroVideo video="https://player.vimeo.com/video/459952948?api=1&amp;autoplay=1&amp;background=1&amp;loop=1&amp;muted=1" />
           <ProjectTextRight
             header="Branded animations"
             body="Some finishing touches to the UI included some extra brand enforcers to tie everything together. The example above is a loader animation which takes a playful approach of the N from the new logo, which swaps between the two primary brand colors."
           />
+          <WorkHeroVideo video="https://player.vimeo.com/video/463651346?api=1&amp;autoplay=1&amp;background=1&amp;loop=1&amp;muted=1" />
           <ProjectImage image={usn4} alt="USN Branding" />
           <ProjectTextLeft
             header="Brand guidelines"
