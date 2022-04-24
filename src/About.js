d89e871c9ce7ddacb2706415664096d597c22076
nickhart00 <31710907+nickhart00@users.noreply.github.com>
2022-04-24 01:58:52 -0600

    updated text tyles

--- src/About.js
+++ src/About.js
@@ -15,7 +15,7 @@ function About(props) {
         detail4="UX/UI Design"
         detail5="Brand & Identity"
         header="Hi there & welcome!"
-        body="I have worked on many satisfying projects ranging from apps, to websites, to animations and identities, all of which keep me hungry for more! I started my career in Detroit, Michigan but currently reside in Boulder, Colorado where I continue to grow professionally, but am also blessed to live a rewarding life in the mountains! Smart, elegant design and nature are my lifeblood."
+        body="I have worked on many satisfying projects ranging from apps, to websites, to animations and identities—all of which keep me hungry for more! I started my career in Detroit, Michigan but currently reside in Boulder, Colorado where I continue to grow professionally, but am also blessed to live a rewarding life in the mountains! Smart, elegant design and nature are my lifeblood."
       />
     </div>
   );
