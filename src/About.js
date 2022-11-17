8dc089cf52bb0449f5a44de0428241007ce69fba
nickhart00 <31710907+nickhart00@users.noreply.github.com>
2022-11-16 18:40:51 -0700

    changed copy

--- src/About.js
+++ src/About.js
@@ -15,7 +15,7 @@ function About(props) {
         detail4="Design Systems"
         detail5="Brand & Identity"
         header="Product designer"
-        body="I have 6+ years in designing and shipping products for mobile and web that have generated positive results in industries such as: Delivery, Healthcare, E-commerce, Music, Entertainment, Web3 and more! I love being an IC with a strong level of ownership in the work that I do. I'm obsessed with craft, compelling experiences, and strategic innovation, a trait that makes its way into my personal life as well. Currently I live in Longmont, Colorado and in my free time enjoy learning new skills and discovering new passions that inspire me. :) "
+        body="I have 6+ years in designing and shipping products for mobile and web that have generated positive results in industries such as: Delivery, Healthcare, E-commerce, Music, Entertainment, Web3 and more! I love being an IC with a strong level of ownership in the work that I do. I'm obsessed with craft, compelling experiences, and strategic innovation—a trait that makes its way into my personal life as well. Currently I live in Longmont, Colorado and in my free time enjoy learning new skills and discovering new passions that inspire me. :)"
       />
     </div>
   );
